a5227b54cd1a59c9a340c2d254b60d3f5bf723e1
Philip Collins <Philip.Collins@unh.edu>
2014-05-17 14:37:22 -0400

    added README LICENSE

--- disk_usage.pl
+++ disk_usage.pl
@@ -6,4 +6,9 @@ use FindBin qw($Bin);
 chdir $Bin;
 my @lines = `du -h --max-depth=2 BACKUPS`;
 @lines = sort map { /^(\S+)\s+(.*)$/; "$2\t$1" } @lines;
-print join("\n", @lines)."\n";
+print join("\n", @lines)."
+
+# to delete a backup set:
+rm -rf BACKUPS/backupname/20140517134528
+
+";
